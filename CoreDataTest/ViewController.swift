0e2c0dc6314da46c773ec59347c62aa99c7ffff8
Mac <sudiptoroy@sudipto.local>
2020-01-23 16:18:35 +0600

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Sudipto Roy on 1/23/20.
+//  Copyright © 2020 Code-X Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
